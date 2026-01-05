a61a0a5ea475e441630df3ad5b9c5bc92665c29d
EphraimCompEng <madugba.finance@proton.me>
2026-01-05 06:43:41 +0000

    Modified to notify users of available choices

--- src/country_input.py
+++ src/country_input.py
@@ -7,14 +7,27 @@ def get_user_input():
         print("Country cannot be empty.")
 
     # Ask for data points
-    print("Enter data points (press Enter on empty input to finish):")
+    print(
+        "Enter data points (press Enter on empty input to finish):\
+        \n(Enter 'help' for all data points)\n"
+    ) # Notify users with options via 'help'-- Ephraim
     data_points = []
 
     while True:
         data = input("> ").strip()
         if data == "":
             break
-        data_points.append(data)
+
+        # Added options so users know valid inputs -- Ephraim
+        if data.lower() == "help":
+            print("\noptions:\n"
+                "name","population","region","capital",
+                "area(km^2)","sunrise","sunset", "timezone","\n"
+            )
+
+        # Added split in case users list items on one line -- Ephraim
+        # Changed append to += as split returns list -- Ephraim
+        data_points += data.split(" ")
 
     return {
         "country": country,
